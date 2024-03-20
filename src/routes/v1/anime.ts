bd1555ad5e9879cbe48fde03a64f553e031984bc
Tobias Kvist Hjuler <84204734+Tohjuler@users.noreply.github.com>
2024-03-20 19:37:40 +0000

    Added /anime/currently-tracking

--- src/routes/v1/anime.ts
+++ src/routes/v1/anime.ts
@@ -237,4 +237,60 @@ route.openapi(unsubscribeRoute, async (c) => {
     return res ? res : c.json({ success: true });
 });
 
+const currentlyTrackingRoute = createRoute({
+    method: "get",
+    path: "/currently-tracking",
+    responses: {
+        200: {
+            content: {
+                "application/json": {
+                    schema: z.array(
+                        z.object({
+                            id: z.string(),
+                            title: z.string().nullable(),
+                            episodes: z.number(),
+                            totalEpisodes: z.number(),
+                            status: z.string(),
+                            createdAt: z.string(),
+                            updatedAt: z.string(),
+                        })
+                    ),
+                },
+            },
+            description: "Ok Response",
+        },
+        500: {
+            content: {
+                "application/json": {
+                    schema: z.object({
+                        error: z.string(),
+                    }),
+                },
+            },
+            description: "Internal Server Error",
+        },
+    },
+    tags: ["Anime"],
+});
+
+route.openapi(currentlyTrackingRoute, async (c) => {
+    const animes = await db.anime
+        .findMany()
+        .then(async (res) => Promise.all(res.map(async (ani) => ({
+            id: ani.id,
+            title: ani.title,
+            episodes: await db.episode.count({ where: { animeId: ani.id } }),
+            totalEpisodes: ani.totalEps,
+            status: ani.status,
+            createdAt: ani.createdAt.toISOString(),
+            updatedAt: ani.updatedAt.toISOString(),
+        }))))
+        .catch((e) => {
+            Sentry.captureException(e);
+            return null;
+        });
+
+    return animes ? c.json(animes) : c.json({ error: "An error occurred" }, 500);
+});
+
 export default route;
